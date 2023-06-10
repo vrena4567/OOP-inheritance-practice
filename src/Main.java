6e3d167d5040058c0fe6583602d2baaefc220b82
vrena4567 <v.rena4567@gmail.com>
2023-06-10 15:53:53 +0200

    ready

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@
 public class Main {
     public static void main(String[] args) {
         // Ezt nyugodtan módosítsd!
-        Person person = new Person();
-        Staff staff = new Staff();
-        Student student = new Student();
+        Staff staff = new Staff("Anna", "Balaton", "Zrínyi Miklós Ált Isk", 260.000);
+        Student student = new Student("Zoli", "Gyál", "jóTanulóProgram", 3, 100.000);
+
+        System.out.println(staff);
+        System.out.println(student);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,29 @@
 public class Person {
+    protected String name;
+    protected String address;
+
+    public Person(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String toString() {
+        return "Person[" + "name=" + name + ", address=" + address + "]";
+    }
 }
